a774ebd00fdb6a6e65a3d24fd2d91fe92860f89d
godmorn <115038963+godmorn@users.noreply.github.com>
2023-05-16 16:53:24 +0400

    Add files via upload

---
+++ Frontend 2.5/data.js
@@ -0,0 +1,20 @@
+var Markers = [
+  [
+    "Корпус 1",
+    53.209756,
+    50.124466,
+    "https://avatars.mds.yandex.net/get-altay/1920875/2a00000174f188fc34e5d0c4f074ae617f93/XXXL",
+  ],
+  [
+    "Главный корпус",
+    53.20875,
+    50.124451,
+    "https://avatars.mds.yandex.net/get-altay/1920875/2a00000174f188fc34e5d0c4f074ae617f93/XXXL",
+  ],
+  [
+    "Корпус 8",
+    53.20935,
+    50.125886,
+    "https://avatars.mds.yandex.net/get-altay/1920875/2a00000174f188fc34e5d0c4f074ae617f93/XXXL",
+  ],
+];
